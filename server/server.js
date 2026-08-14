8caa3178621ee8d8fe7cca973d0d0ea2c66e6a87
ashwini01-12 <23071159@ycce.in>
2026-08-14 21:55:14 +0530

    Authorization and Authentication implemented

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth" , authRoutes);
 
 const PORT = process.env.PORT || 8080;
 
